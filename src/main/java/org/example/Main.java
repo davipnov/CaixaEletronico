e189bade64fd7d4a407afcd094c0f9afb1beddda
Davi P Novaes <davi_novaes@estudante.sesisenai.org.br>
2026-07-22 20:41:11 -0300

    feat: Adiciona interação com usuário

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,31 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
         CaixaEletronico caixa = new CaixaEletronico();
 
-        //Scanner
+        int opcao;
+
+        do{
+
+            System.out.println("1 - Saldo");
+            System.out.println("2 - Saque");
+            System.out.println("3 - Sair");
+
+            opcao = sc.nextInt();
+
+            switch(opcao){
+                case 1:
+                    caixa.verficarSaldo();
+                    break;
+                case 2:
+                    System.out.println("Digite o valor do saque: ");
+                    int valor = sc.nextInt();
+                    caixa.realizarSaque(valor);
+                    break;
+
+            }while (opcao !=3);
+        }
 
     }
 }
