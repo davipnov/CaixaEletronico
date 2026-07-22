c30cff7eb3dc3b96a6537e0b88927dc4289be3fb
Davi P Novaes <davi_novaes@estudante.sesisenai.org.br>
2026-07-22 19:42:51 -0300

    feat: Adiciona valores em estoque

---
+++ src/main/java/org/example/CaixaEletronico.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CaixaEletronico {
+    int nota100 = 5;
+    int nota50 = 10;
+    int nota20 = 10;
+    int nota10 = 20;
+
+
+
+
+
+}
